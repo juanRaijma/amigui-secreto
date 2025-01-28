2313ddefad78694ec118af77fb51674284c1b885
juanr <juan.raijmakers@gmail.com>
2025-01-28 18:22:02 -0300

    se afrego funcion para afregar datos a lista

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// crear variable id
+
+let amigo = [];
+//crear funcion para agregar datos a la variable
+function agregarAmigo() {
+    
+    let dato = document.getElementById ("amigo"). value;
+    if (dato == 0) alert ("ingrese dato valido");
+    else {amigo.push(dato)}
+
+    console.log(amigo);
+    console.log(dato);
+ 
+  
+}
+    
+
+    
+
+   
+
+   
+
+        
+    
+
+
+   
+
+
+
